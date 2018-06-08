9ef09d670d869d785b25818115a35a52efa8c0a1
Dinesh Trivedi <dinesh.dart@gmail.com>
2018-06-08 17:50:48 +0200

    Enf of tutorial

---
+++ README.md
@@ -0,0 +1,3 @@
+# VueJS - Skill list tutorial
+
+This code was created following this [tutorial from coursetro](https://coursetro.com/courses/23/Vue-Tutorial-in-2018---Learn-Vue.js-by-Example)
---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1 v-if="their_name">Hello {{ their_name }}, This is an about page</h1>
+    <h1 v-else>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  },
+}
+</script>
